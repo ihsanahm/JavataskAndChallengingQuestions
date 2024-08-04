f0a9b899e329bee07a94aa63c787448ca74e62aa
ihsanahm <142923280+ihsanahm@users.noreply.github.com>
2024-08-04 19:12:04 +0500

    Update ChallengeSeven.java
    resolve Challenge number seven of the day

--- src/ChallengeSeven.java
+++ src/ChallengeSeven.java
@@ -1,6 +1,20 @@
 public class ChallengeSeven {
     public static void main(String[] args) {
         //This is the challenge to swap two numbers
+        int num=120;
+        int num2=100;
+        int num3;
+        System.out.println("This the number one : "+num);
+        System.out.println("This the number two : "+num2);
+        num3=num;
+        System.out.println("For swaping number assign to third number number one :"+num3);
+        num=num2;
+        System.out.println("Number swap assign th number two value to one  "+num);
+        num2=num3;
+        System.out.println("Number swap assign the number one value to number two :"+num2);
+
+
+        
 
     }
 }
